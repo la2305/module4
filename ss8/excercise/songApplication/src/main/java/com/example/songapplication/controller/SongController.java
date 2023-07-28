fbdd36436ebf1a5cbf722811dccf52a50306c667
la2305 <lequangphuoc2305@gmail.com>
2023-07-28 15:20:24 +0700

    up

--- ss8/excercise/songApplication/src/main/java/com/example/songapplication/controller/SongController.java
+++ ss8/excercise/songApplication/src/main/java/com/example/songapplication/controller/SongController.java
@@ -3,11 +3,18 @@ package com.example.songapplication.controller;
 import com.example.songapplication.dto.SongDto;
 import com.example.songapplication.model.Song;
 import com.example.songapplication.service.ISongService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,10 +28,60 @@ public class SongController {
         return "/list";
     }
     @GetMapping("/create")
-    public String create(Model model){
+    public String showCreate(Model model){
         SongDto songDto = new SongDto();
         model.addAttribute("songDto",songDto);
         return "/create";
+    }
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute SongDto songDto,
+                         BindingResult bindingResult,
+                         Model model,
+                         RedirectAttributes redirectAttributes){
+        Song song = new Song();
+        new SongDto().validate(songDto,bindingResult);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("songDto",songDto);
+            return "/create";
+        }
+        BeanUtils.copyProperties(songDto,song);
+        boolean check = songService.addSong(song);
+        if (check){
+            redirectAttributes.addFlashAttribute("msg","thêm mới thành công");
+            return "redirect:/list";
+        }else {
+            redirectAttributes.addFlashAttribute("msg","thêm mới không thành công");
+            return "redirect:/list";
+        }
+    }
+    @GetMapping("/edit/{id}")
+    public String showEdit(@PathVariable int id, Model model){
+        Song song = songService.selectById(id);
+        SongDto songDto = new SongDto();
+        BeanUtils.copyProperties(song,songDto);
+        model.addAttribute("songDto",songDto);
+        return "/edit";
+    }
+    @PostMapping("/edit")
+    public String edit(@Valid @ModelAttribute SongDto songDto,
+                       BindingResult bindingResult,
+                       Model model,
+                       RedirectAttributes redirectAttributes){
+        new SongDto().validate(songDto,bindingResult);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("songDto",songDto);
+            return "/edit";
+        }
+        Song song = new Song();
+        BeanUtils.copyProperties(songDto,song);
+        boolean check = songService.addSong(song);
+        if (check){
+            redirectAttributes.addFlashAttribute("msg","Chỉnh sửa thành công");
+            return "redirect:/list";
+        }else {
+            redirectAttributes.addFlashAttribute("msg","Chỉnh sửa thất bại");
+            return "redirect:/list";
+        }
     }
 
 }
--- ss8/excercise/songApplication/src/main/java/com/example/songapplication/dto/SongDto.java
+++ ss8/excercise/songApplication/src/main/java/com/example/songapplication/dto/SongDto.java
@@ -1,6 +1,9 @@
 package com.example.songapplication.dto;
 
-public class SongDto {
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class SongDto implements Validator {
     private int id;
     private String name;
     private String nameSinger;
@@ -54,4 +57,34 @@ public class SongDto {
         this.type = type;
     }
 
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SongDto songDto = (SongDto) target;
+        if (songDto.getName().equals("")){
+            errors.rejectValue("name",null,"Không để trống tên bài hát");
+        } else if (songDto.getName().length()>800) {
+            errors.rejectValue("name",null,"Tên không được vượt quá 800 kí tự");
+        }else if (songDto.getName().matches("^[^<>{}\\\"/|;:.,~!?@#$%^=&*\\\\]\\\\\\\\()\\\\[¿§«»ω⊙¤°℃℉€¥£¢¡®©0-9_+]*$")){
+            errors.rejectValue("name",null,"tên không chứa kí tự đặc biệt nhé");
+        }
+        if (songDto.getNameSinger().equals("")){
+            errors.rejectValue("nameSinger",null,"Không để trống tên ca sĩ");
+        } else if (songDto.getName().length()>300) {
+            errors.rejectValue("nameSinger",null,"Tên không được vượt quá 300 kí tự");
+        }else if (songDto.getName().matches("^[^<>{}\\\"/|;:.,~!?@#$%^=&*\\\\]\\\\\\\\()\\\\[¿§«»ω⊙¤°℃℉€¥£¢¡®©0-9_+]*$")){
+            errors.rejectValue("nameSinger",null,"tên không chứa kí tự đặc biệt nhé");
+        }
+        if (songDto.getType().equals("")){
+            errors.rejectValue("name",null,"Không để trống thể loại nhạc");
+        } else if (songDto.getType().length()>1000) {
+            errors.rejectValue("name",null,"Tên không được vượt quá 1000 kí tự");
+        }else if (songDto.getType().matches("^[^<>{}\\\"/|;:.~!?@#$%^=&*\\\\]\\\\\\\\()\\\\[¿§«»ω⊙¤°℃℉€¥£¢¡®©0-9_+]*$")){
+            errors.rejectValue("name",null,"tên không chứa kí tự đặc biệt nhé, có thể chấp nhận dấu ,");
+        }
+    }
 }
